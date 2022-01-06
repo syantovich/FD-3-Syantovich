9d97a167835b0ee6a39854c5ace567dce78d1d28
syantovich <72738946+syantovich@users.noreply.github.com>
2022-01-06 10:42:06 +0300

    3

--- RainbowFrame/components/RaindowFrame.js
+++ RainbowFrame/components/RaindowFrame.js
@@ -6,7 +6,7 @@ class RaindowFrame extends React.Component{
 
     render(){
         let div=this.props.children;
-        for(let i=this.props.colors.length;i>=0;i--){
+        for(let i=this.props.colors.length-1;i>=0;i--){
             div=<div style={{border:"solid 1px "+this.props.colors[i],padding:"10px"}}>
             {div}
           </div>;
