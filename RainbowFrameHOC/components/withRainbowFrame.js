e656c6b3608eb5b08cacbb31beec17ef0d00181f
syantovich <72738946+syantovich@users.noreply.github.com>
2022-01-11 00:36:25 +0300

    error Fragment

--- RainbowFrameHOC/components/withRainbowFrame.js
+++ RainbowFrameHOC/components/withRainbowFrame.js
@@ -4,16 +4,16 @@ import React from 'react';
  
 function withRainbowFrame(arrColors){
     return Component =>{
-        let div=false;
-        for(let i=arrColors.length-1;i>=0;i--){
-            div=<div style={{border:"solid 1px "+arrColors[i],padding:"10px"}}>
-            {div}
-          </div>;
-        }
         return props=>{
+            let div=props.children;
+            for(let i=arrColors.length-1;i>=0;i--){
+                div=<div style={{border:"solid 1px "+arrColors[i],padding:"10px"}}>
+                {div}
+              </div>;
+            }
             console.log(props);
             return (<Component>
-                <p>{props.children}</p>
+                <p>{div}</p>
             </Component>)
         };
     }
